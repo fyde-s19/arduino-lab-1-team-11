cda8978ac3f67abb19f73c3b7e77af32ca3e9b90
sriraman2000 <47581572+sriraman2000@users.noreply.github.com>
2019-02-26 13:27:24 -0600

    Add files via upload
    This is Part 4

--- Lab_1.ino
+++ Lab_1.ino
@@ -4,6 +4,21 @@ void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
 }
 
+void dimmer(int freq, int duty)
+{
+  int period;
+  int onTime;
+  int offTime;
+
+  period = 1000/freq; //delay() is in milliseconds
+  onTime = period * duty /100; 
+  offTime = period - onTime;
+  digitalWrite(LED_BUILTIN, HIGH); // turn on LED with HIGH voltage
+  delay(onTime); // wait for the interval
+  digitalWrite(LED_BUILTIN, LOW); // turn on LED with LOW voltage
+  delay(offTime); // wait for a second 
+  
+}
 void timedBlink(int interval)
 {
   digitalWrite(LED_BUILTIN, HIGH); // turn on LED with HIGH voltage
@@ -15,8 +30,11 @@ void timedBlink(int interval)
 
 void loop() {
   // put your main code here, to run repeatedly:
-  timedBlink(1);
-  timedBlink(2);
-  timedBlink(3);
+  //timedBlink(1);
+  //timedBlink(2);
+  //timedBlink(3);
+  int iterator = 0;
+  dimmer(100 - 10*iterator, iterator);
+  
   
 }
